16a63816247ba1836af9753dc0f6e116a6c30466
Ravi Maharjan <robbiemhzn@gmail.com>
2017-03-27 13:58:14 +0545

    Test

---
+++ NagarjunaSpring/src/main/java/com/nagarjuna/utils/DatabaseConfig.java
@@ -0,0 +1,61 @@
+package com.nagarjuna.utils;
+
+import com.nagarjuna.models.AppUser;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by max on 3/27/2017.
+ */
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Bean(name = "DataSource")
+    public HikariDataSource getDataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+        dataSource.addDataSourceProperty("databaseName", "nagarjuna_db");
+        dataSource.addDataSourceProperty("portNumber", "3306");
+        dataSource.addDataSourceProperty("serverName", "127.0.0.1");
+        dataSource.addDataSourceProperty("user", "root");
+        dataSource.addDataSourceProperty("password", "");
+        return dataSource;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager manager = new HibernateTransactionManager();
+        manager.setSessionFactory(hibernate5SessionFactoryBean().getObject());
+        return manager;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean hibernate5SessionFactoryBean() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource((DataSource) appContext.getBean("DataSource"));
+        localSessionFactoryBean.setAnnotatedClasses(
+                AppUser.class
+        );
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+
+        localSessionFactoryBean.setHibernateProperties(properties);
+        return localSessionFactoryBean;
+    }
+}
